c1f0f0ba97e6a65d81109654cc92137dd76d53da
antoluduenabereziuk21 <antolube20@gmail.com>
2023-08-17 15:30:58 -0300

    added in constants file api.constansts URL RICK&MORTI

---
+++ src/constants/Api.constants.js
@@ -0,0 +1,16 @@
+/**
+ * aqui alojermos los datos de una api
+ */
+
+export const API_RM={
+
+    URL:"https://rickandmortyapi.com/api",
+    
+    CHARACTERS : function(){
+        return `${this.URL}/character`
+    },
+    CHARACTERS_BY_ID : function(){
+        return `${this.URL}/character/${id}`
+    }
+}
+    
