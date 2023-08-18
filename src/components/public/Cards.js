d0efbe651392d6b84a76e386e8f6730c71cf1529
antoluduenabereziuk21 <antolube20@gmail.com>
2023-08-18 12:17:31 -0300

    genereting Cards from API

--- src/components/public/Cards.js
+++ src/components/public/Cards.js
@@ -2,23 +2,28 @@ import React from 'react'
 import  {Card} from './Card'
 /**
  * 
- * podemos mediante props ver los que nos llega
- * console.log(props);
+ * utilizaremos destructuring para recibir lo que nos esta
+ * llegando podriamos usar props tambien
  */
- export const Cards = (props) => {
-  //console.log(props);
+ export const Cards = ( { mascotas }) =>  {
+  /**
+   * generaremos tantos componente  Card como necesitemos
+   * mediante la variable cardList ,totamos mascotas que
+   * es un Array , le aplicaremos el map para generar otro
+   * array , y dentro de map llamamos a una callback
+   * Function que toma los elementos bajo la variable m 
+   * y devolvera un CardComponent(generando 20 Cartas)
+   * Ahora usando el jsx cardList ,lo rederizaremos 
+   * mediante binding dentro del div 
+   */
+
+  const  cardsList = mascotas.map((m) => <Card/> );
   return (
     <div className="album py-5 bg-body-tertiary">
           <div className="container">
       
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-             <Card/>
-             <Card/>
-             <Card/>
-             <Card/>
-             <Card/>
-             <Card/>
-             
+              {cardsList}
             </div>
           </div>
         </div>
